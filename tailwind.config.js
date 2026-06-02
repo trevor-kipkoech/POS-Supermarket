3fadbae541987fe7a70a42027b7b264aa5138800
Trevor Kipkoech <152680464+trevor-kipkoech@users.noreply.github.com>
2026-06-02 23:45:40 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class', // 🌟 ADDED: Enabled class-driven dark mode switching
   theme: {
     extend: {
       fontFamily: {
